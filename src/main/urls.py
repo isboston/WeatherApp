83c1b6dc5a41d25047a8e5f6468e6f7cd04db23c
isboston <itvibn@mail.ru>
2021-01-24 14:07:54 +0300

    +

--- src/main/urls.py
+++ src/main/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about-us'),
     path('weather', views.weather, name='weather'),
-    path('accounts/', include('django.contrib.auth.urls')),
-
+    path('register', views.register, name='register'),
+    path('login', views.user_login, name='login'),
+    path('logout', views.user_logout, name='logout'),
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -1,6 +1,11 @@
+from .forms import TaskForm, UserRegisterForm, UserLoginForm
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib import messages
 from .models import Task
-from .forms import TaskForm
+
+
+
 
 def index(request):
     tasks = Task.objects.all()
@@ -27,3 +32,34 @@ def weather(request):
         'error': error
     }
     return render(request, 'main/weather.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'You have successfully registered')
+            return redirect('login')
+        else:
+            messages.error(request, 'Registration error')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'main/register.html', {'form': form})
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserLoginForm()
+    return render(request, 'main/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
